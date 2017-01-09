0c63f7583e4e60d6b6c8771901a645c67bd40443
Rodwyn Moreno <rodwyn.moreno@the-cocktail.com>
2017-01-09 12:02:37 -0600

    Added scripts task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,22 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+
+//Styles
+gulp.task('styles', function () {
+  console.log('staringstyles task');
+});
+
+//Scripts
+gulp.task('scripts', function () {
+  console.log('Staring scripts task.');
+  return gulp.src('public/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'))
+});
+
+// Images
+
+
+gulp.task('default', function () {
+
+});
