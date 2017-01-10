91ff442b918b9ba47ba62e8671a71f19c22d9245
Rodwyn Moreno <rodwyn.moreno@the-cocktail.com>
2017-01-09 18:32:29 -0600

    Added screen shot task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var localScreenshots = require('gulp-local-screenshots');
+
+gulp.task('screens', function () {
+  gulp.src('dist/index.html')
+  .pipe(localScreenshots({
+    width: ['1600', '1000', '480', '320'],
+    path: 'dist/'
+  }));
+});
+
 
 //File paths
 var SCRIPTS_PATH = 'source/scripts/**/*.js';
