331dfcd18704e1047c438a31b4b466d49d834157
Rodwyn Moreno <rodwyn.moreno@the-cocktail.com>
2017-01-11 12:20:24 -0600

    Added screen shoots task.

---
+++ gulp/screenshoots.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var gulp = require('gulp'),
+    del = require('del'),
+    plugins = require('gulp-load-plugins')({
+        pattern: ['gulp-*', 'gulp.*'],
+        replaceString: /^gulp(-|\.)/,
+        lazy: false
+    }),
+    config = require('./config/config.json');
+    // ,
+    // ,
+    // scripts = require('./config/scripts.json'),
+    // options = {
+    //   "lint": true,
+    // };
+
+gulp.task('screens', ['clean-screens'], function() {
+  gulp.start('generate-screens');
+});
+
+gulp.task('clean-screens', function() {
+    del('screens/');
+});
+
+gulp.task('generate-screens', function() {
+  gulp.src(config.path.build + '/**/*.html')
+    .pipe(plugins.localScreenshots({
+      width: config.screenSizes,
+      path: config.path.build
+    }));
+});
